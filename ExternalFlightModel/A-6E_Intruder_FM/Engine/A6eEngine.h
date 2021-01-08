8af778249b8e925e0145f1f967e885ac8d815282
CorsairCat <1274056321@qq.com>
2021-01-08 23:26:30 +0000

    init engine control class

---
+++ ExternalFlightModel/A-6E_Intruder_FM/Engine/A6eEngine.h
@@ -0,0 +1,16 @@
+#ifndef _A6EENGINE_H_
+#define _A6EENGINE_H_
+
+#include "../A6E_FM_Utility.h"
+
+// use for calculate thrust and get data from fuel.
+class A6eEngineSystem
+{
+private:
+    /* data */
+
+public:
+
+};
+
+#endif
---
+++ ExternalFlightModel/A-6E_Intruder_FM/Engine/A6eFuel.h
@@ -0,0 +1,17 @@
+#ifndef _A6EFUEL_H_
+#define _A6EFUEL_H_
+
+#include "../A6E_FM_Utility.h"
+
+// for Fuel Consumpsion and usage
+// consider if we need it
+class A6eFuelSystem
+{
+private:
+    /* data */
+
+public:
+
+};
+
+#endif
